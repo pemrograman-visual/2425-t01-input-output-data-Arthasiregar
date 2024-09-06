4c6179263141780e0df1f8da739643dae1c2d6d9
Arthasiregar <arthasiregar10@gmail.com>
2024-09-06 11:35:35 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,1 +1,28 @@
-// NIM - Your Name
+// 12S24010 - Artha Liebe Siregar
+// 12S24054 - Rahel Juri Elisabet Silaban
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penulis, penerbit, formatbukuelektronik;
+        int tahunTerbit;
+        double hargaPembelian, minimummargin, stok;
+        double rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunTerbit = Integer.parseInt(input.nextLine());
+        penerbit = input.nextLine();
+        formatbukuelektronik = input.nextLine();
+        hargaPembelian = Double.parseDouble(input.nextLine());
+        minimummargin = Double.parseDouble(input.nextLine());
+        stok = Double.parseDouble(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunTerbit + "|" + penerbit + "|" + formatbukuelektronik + "|" + hargaPembelian + "|" + minimummargin + "|" + stok + "|" + rating);
+    }
+}
